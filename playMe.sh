358f16edbf687d9ea1a34b39b945de29f899c599
Andrew Donnelly <thetallerbrother@outlook.com>
2026-04-05 20:59:21 -0700

    Bug fixes, still won't close down old windows.

--- playMe.sh
+++ playMe.sh
@@ -6,9 +6,9 @@ if [ -f config.cfg ]; then
     source config.cfg # Should define SONG_FILE
 fi
 
-: ${numSongs:=-1}
-: ${timePerSong:=5m}
-: ${SONG_FILE:="songs.txt"}
+
+song_time="$DEFAULT_SONG_TIME" # set to default, overriden later
+: ${SONG_FILE:="songURLs.txt"}
 
 usage () {
     cat << EOF
@@ -34,14 +34,13 @@ while getopts "ht:n:" opt; do
         t)
             regex="^[0-9]{1,3}[smh]$"
             if [[ $OPTARG =~ $regex ]]; then
-                timePerSong="$OPTARG"
+                song_time="$OPTARG"
             else
                 echo "ERROR: $OPTARG is an invalid time format."
                 exit 1
             fi
             ;;
         n)
-            # Changed INPUT to OPTARG
             if [[ $OPTARG =~ ^[0-9]+$ ]] && [ "$OPTARG" -gt 0 ]; then
                 numSongs=$OPTARG
             else
@@ -51,7 +50,8 @@ while getopts "ht:n:" opt; do
             ;;
     esac
 done
-# MUST close getopts loop with 'done'
+
+: ${counter:=-1}
 shift $((OPTIND -1))
 
 # 3. Setup
@@ -60,7 +60,7 @@ if [ ! -s "$SONG_FILE" ]; then
 	echo "separated by newlines (when you press enter) to add those"
 	echo "to your playlist"
 	echo "example https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1&pp=ygUIcmlja3JvbGygBwE%3D"
-touch "$SONG_FILE"
+fi
 numSongsInList=$(wc -l < "$SONG_FILE")
 
 if [ "$numSongsInList" -eq 0 ]; then
@@ -74,22 +74,21 @@ counter=0
 while true; do
     # Calculate random line number
     randomNum=$(( ($RANDOM % numSongsInList) + 1 ))
-    
+
     # Get the URL
     URL=$(awk "NR==$randomNum" "$SONG_FILE")
-    
+
     echo "[$((counter+1))] Playing: $URL"
-    
+
     # Open Firefox in background
     firefox --new-window "$URL" &
     BROWSER_PID=$!
-    
     # Wait and then kill
-    sleep "$timePerSong"
+    sleep "$song_time"
     kill $BROWSER_PID 2>/dev/null
-    
+
     ((counter++))
-    
+
     # Check if we've played enough songs
     if [ "$counter" -eq "$numSongs" ]; then
         echo "Finished playing $numSongs songs."
