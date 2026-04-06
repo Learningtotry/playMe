2c7e4e9b94a459334ea32ade019290ce55443a8d
Andrew Donnelly <thetallerbrother@outlook.com>
2026-04-05 20:37:54 -0700

    Modified with help of AI. Cleaned up code, added functionality for selecting number of songs to play.

--- playMe.sh
+++ playMe.sh
@@ -1,70 +1,98 @@
 #!/bin/bash
 
-
-usage () {
-	cat << EOF
-Usage: playMe [-h] [-t TIMEVAR]
-
-A song player that will randomly play songs with URLs you list in a list specified in another file.
-
-Options:
-	-h		Display this help message and exit
-	-t 		Specify the amount of time to play each song
-	-n		Specify number of songs to play before closing
-
-Examples:
-	playMe
-	playMe -t 4m
-EOF
-	exit 0
-}
-
-
-while getopts "ht:n:" opt; do
-	case $opt in
-	h) usage ;;
-	t)
-	regex="^[0-9]{1,3}[smh]$"
-	if [[ $OPTARG =~ $regex ]]; then
-		timePerSong="$OPTARG"
-	else
-		echo 'ERROR: "$opt" is an invalid argument for -t parameter'
-		echo 'Example: playMe -t 5h'
-	fi
-	;;
-	n)
-		if [[ $OPTARG =~ ^[0-9]+$ ]] && [ "$INPUT" -gt 0 ]; then
-			numSongs=$OPTARG
-		else
-			echo "Error, argument to -n must be a positive integer"
-			exit 1
-		fi
-	;;
-	esac
+# 1. Load config and set defaults BEFORE getopts
+# This allows the user's flags to OVERRIDE the config file.
+if [ -f config.cfg ]; then
+    source config.cfg # Should define SONG_FILE
+fi
 
 : ${numSongs:=-1}
-source config.cfg # holds SONG_FILE
+: ${timePerSong:=5m}
+: ${SONG_FILE:="songs.txt"}
+
+usage () {
+    cat << EOF
+Usage: playMe [-h] [-t TIME] [-n COUNT]
+
+A song player that opens random URLs from a list.
+
+Options:
+    -h      Display this help message and exit
+    -t      Time per song (e.g., 30s, 5m, 1h)
+    -n      Number of songs to play before stopping
+
+Examples:
+    playMe -t 4m -n 10
+EOF
+    exit 0
+}
+
+# 2. Argument Parsing
+while getopts "ht:n:" opt; do
+    case $opt in
+        h) usage ;;
+        t)
+            regex="^[0-9]{1,3}[smh]$"
+            if [[ $OPTARG =~ $regex ]]; then
+                timePerSong="$OPTARG"
+            else
+                echo "ERROR: $OPTARG is an invalid time format."
+                exit 1
+            fi
+            ;;
+        n)
+            # Changed INPUT to OPTARG
+            if [[ $OPTARG =~ ^[0-9]+$ ]] && [ "$OPTARG" -gt 0 ]; then
+                numSongs=$OPTARG
+            else
+                echo "Error: -n must be a positive integer."
+                exit 1
+            fi
+            ;;
+    esac
+done
+# MUST close getopts loop with 'done'
+shift $((OPTIND -1))
+
+# 3. Setup
+if [ ! -s "$SONG_FILE" ]; then
+	echo "It seems that $SONG_FILE is empty, please paste some URLs"
+	echo "separated by newlines (when you press enter) to add those"
+	echo "to your playlist"
+	echo "example https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1&pp=ygUIcmlja3JvbGygBwE%3D"
+touch "$SONG_FILE"
+numSongsInList=$(wc -l < "$SONG_FILE")
+
+if [ "$numSongsInList" -eq 0 ]; then
+    echo "Error: Your song list ($SONG_FILE) is empty!"
+    exit 1
+fi
 
 counter=0
-: ${timePerSong:=5m} # number of minutes to continue playing one song
-touch $SONG_FILE
-numSongsInList=$(wc -l $SONG_FILE | awk '{print $1}' -)
 
-
-
-while true
-do
-	randomNum=$(( $RANDOM % $numSongsInList + 1 ))
-	URL=$(awk -v randomNum="$randomNum" 'NR==randomNum { print $0 }' $SONG_FILE)
-	firefox --new-window "$URL" &
-	BROWSER_PID=$!
-	sleep $timePerSong
-	kill $BROWSER_PID
-	((counter++))
-	if [[ $counter -eq $numSongs ]]; then
-		echo "Done"
-		exit 0
-	fi
+# 4. Main Loop
+while true; do
+    # Calculate random line number
+    randomNum=$(( ($RANDOM % numSongsInList) + 1 ))
+    
+    # Get the URL
+    URL=$(awk "NR==$randomNum" "$SONG_FILE")
+    
+    echo "[$((counter+1))] Playing: $URL"
+    
+    # Open Firefox in background
+    firefox --new-window "$URL" &
+    BROWSER_PID=$!
+    
+    # Wait and then kill
+    sleep "$timePerSong"
+    kill $BROWSER_PID 2>/dev/null
+    
+    ((counter++))
+    
+    # Check if we've played enough songs
+    if [ "$counter" -eq "$numSongs" ]; then
+        echo "Finished playing $numSongs songs."
+        exit 0
+    fi
 done
-
-
